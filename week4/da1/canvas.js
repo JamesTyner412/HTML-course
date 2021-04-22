3850b1864fd49aed400151d719a5558306081741
JamesTyner412 <jatyner@email.neit.edu>
2021-04-22 14:08:52 -0400

    update day 2

--- week4/da1/canvas.js
+++ week4/da1/canvas.js
@@ -1,30 +1,63 @@
 //defines variable to acess canvas properties by ID
-var canvas=document.getElementById('canvas')
+var canvas = document.getElementById('canvas')
 //Define the drawing context of the Element
 var ctx = canvas.getContext('2d')
 
-//draw rectangle
 
-ctx.fillStyle= "red"
-ctx.strokeStyle= "green"
-ctx.lineWidth= "5"
 
-ctx.strokeRect(30, 30, 100, 100)
-ctx.fillRect(30, 30, 100, 100)
+var galaxy = new Image()
+galaxy.src = "image/galaxy.jpg"
 
-//draw line
-ctx.moveTo(0,0)
-ctx.lineTo(800,600)
-ctx.stroke()
+galaxy.onload = function () {
+    ctx.drawImage(galaxy, 0, 0, 800, 600);
+    //draw rectangle
 
-ctx.moveTo(800,0)
-ctx.lineTo(0,600)
-ctx.stroke()
+    ctx.fillStyle = "blue"
+    ctx.strokeStyle = "green"
+    ctx.lineWidth = "5"
 
-//draw circle
-//ctx.ac(x,y radius, startAngle, endAngle, isCounterClockwise)
-ctx.beginPath()
-ctx.arc(400, 300,50, 0, (3 * Math.PI)/2, false)
-ctx.lineTo(450,250)
-ctx.closePath()
-ctx.fill()
+    ctx.strokeRect(30, 30, 100, 100)
+    ctx.fillRect(30, 30, 100, 100)
+
+    //draw line
+    ctx.moveTo(0, 0)
+    ctx.lineTo(800, 600)
+    ctx.stroke()
+
+    ctx.moveTo(800, 0)
+    ctx.lineTo(0, 600)
+    ctx.stroke()
+
+    //draw circle
+    //ctx.ac(x,y radius, startAngle, endAngle, isCounterClockwise)
+    ctx.beginPath()
+    ctx.arc(400, 300, 50, 0, (3 * Math.PI) / 2, false)
+    ctx.lineTo(450, 250)
+    ctx.closePath()
+    ctx.fill()
+
+    //draw shape
+
+    ctx.fillStyle = '#55ddef'
+    ctx.strokeStyle = "yellow"
+    ctx.beginPath()
+    ctx.moveTo(650, 100)
+    ctx.lineTo(700, 150)
+    ctx.lineTo(675, 2000)
+    ctx.lineTo(625, 200)
+    ctx.lineTo(600, 140)
+    ctx.closePath()
+    ctx.fill()
+    ctx.stroke()
+
+    //draw an image to canvas
+    //create the instance of the image
+    var mario = new Image()
+    //links the source of the image file
+    mario.src = 'image/mario.png'
+    //callback functionloads image and draw it to canvas
+    mario.onload = function () {
+        ctx.drawImage(mario, 600, 300, 40, 80)
+    }
+
+}
